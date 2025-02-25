ee80de7ab94e7b29153fd788aba5df1e2c5dee75
André Maia <andrenmaia@gmail.com>
2025-02-25 00:11:04 -0300

    Fix ts lint issues

--- tests/app/internal-home.spec.ts
+++ tests/app/internal-home.spec.ts
@@ -2,6 +2,7 @@ import { test } from '@playwright/test';
 import { InternalHomePage } from "./internal-home.page";
 import { CurrentUserPage } from "./current-user.page";
 
+// eslint-disable-next-line playwright/expect-expect
 test('welcome phrase is loading after login', async ({ page }) => {
     const user = await new CurrentUserPage(page).getCurrentUser()
     const pageObj = new InternalHomePage(page);
